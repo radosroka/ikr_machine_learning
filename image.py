830551e2ab2f3b17523aae1c61034c779062703f
peett57 <revajpeter57@gmail.com>
2017-04-28 20:13:21 +0200

    correct output format

--- image.py
+++ image.py
@@ -1,5 +1,6 @@
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
+from glob import glob
 import matplotlib.pyplot as plt
 from ikrlib import png2fea
 import scipy.linalg
@@ -50,8 +51,17 @@ average = average / float(img_counter)
 
 test_counter = 0
 
-for i in range(1, 32):
-    test[i] = png2fea("./dev/" + str(i))
+for i in range(1, 2):
+    test[i] = png2fea("./eval/" )
+
+    dir_eval = 'eval/'
+
+    f = glob(dir_eval + '/*.png')
+    for x in range (0, len(f)):
+        f[x] = f[x][:-4]
+        f[x] = f[x].replace('eval\\' , '')
+
+
     for key in test[i]:
         tmp = test[i][key].reshape(-1)
         test_set_list.append(tmp)
@@ -69,6 +79,8 @@ for i in range(len(test_set_list)):
     test_set_list[i] = test_set_list[i] - average
 
 test_matrix = np.array(test_set_list)
+
+
 
 ###### PCA of train
 
@@ -88,7 +100,7 @@ pca = PCA(n_components=num, svd_solver='randomized').fit(train_matrix)
 
 train_vectors = pca.transform(train_matrix)
 eigenfaces = pca.components_.reshape((num, 80, 80))
-print str(len(eigenfaces)) + " << aaaaaaaaaaa"
+#print str(len(eigenfaces)) + " << aaaaaaaaaaa"
 #sorted_vect[:,range(num)]
 
 #mapping = None
@@ -114,6 +126,7 @@ print str(len(eigenfaces)) + " << aaaaaaaaaaa"
 
 test_vectors = pca.transform(test_matrix)
 
+
 #train_vectors = train_vectors.T[1:].T
 #test_vectors = test_vectors.T[1:].T
 
@@ -121,7 +134,35 @@ lr = LogisticRegression(max_iter=1000)
 lr.fit(train_vectors, train_set_names)
 
 print lr.predict(test_vectors)
-print lr.score(test_vectors, test_set_names)
+predict = lr.predict(test_vectors)
+log_prob = lr.predict_log_proba(test_vectors)
+
+#print(predict[0])
+#print(log_prob[0])
+
+#print lr.score(test_vectors, test_set_names)
+
+final = ''
+#print (len(test[1]))
+for x in range(0,len(test[1])):
+
+    predict = lr.predict(test_vectors)
+    log_prob = lr.predict_log_proba(test_vectors)
+
+    final += str(f[x])
+    final += ' '
+    final += str(predict[x])
+    final += ' '
+    for y in range(0, len(log_prob[x])):
+        final += str(log_prob[x][y])
+        final += ' '
+    final += '\n'
+
+output = 'output_images'
+write = open(output , 'w')
+write.write(final)
+write.close()
+
 
 #clf.predict_proba(...)
 #clf.predictlog_proba(....)
