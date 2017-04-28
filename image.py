263a97f228149ff0e54ef4091f589fd6cc79b3f4
Radovan Sroka <rsroka@redhat.com>
2017-04-28 11:46:42 +0200

    using PCA and SVM lib

--- image.py
+++ image.py
@@ -1,8 +1,22 @@
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 from ikrlib import png2fea
 import scipy.linalg
 import numpy as np
-from sklearn.decomposition import PCA
+
+
+def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
+    """Helper function to plot a gallery of portraits"""
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+
 
 train = {}
 test = {}
@@ -15,7 +29,7 @@ train_set_list = []
 test_set_names = []
 test_set_list = []
 
-num = 12 
+num = 20 
 
 img_counter = 0
 
@@ -58,18 +72,22 @@ test_matrix = np.array(test_set_list)
 
 ###### PCA of train
 
-big_matrix = np.concatenate((train_matrix, test_matrix), axis=0)
+#big_matrix = np.concatenate((train_matrix, test_matrix), axis=0)
  
 # C = AA' --> python C = A'A
-cov_matrix = np.cov(big_matrix)
-[evals, evect] = np.linalg.eig(cov_matrix)
+#cov_matrix = np.cov(big_matrix)
+#[evals, evect] = np.linalg.eig(cov_matrix)
 
 #sort eigen vectors
-sorted_args = np.argsort(evals)
-sorted_args = sorted_args[::-1]
-sorted_vals = np.sqrt(np.sort(evals))
-sorted_vect = evect[:,sorted_args]
-train_vectors = sorted_vect[:,range(num)]
+#sorted_args = np.argsort(evals)
+#sorted_args = sorted_args[::-1]
+#sorted_vals = np.sqrt(np.sort(evals))
+#sorted_vect = evect[:,sorted_args]
+
+pca = PCA(n_components=num, svd_solver='randomized').fit(train_matrix)
+
+train_vectors = pca.transform(train_matrix)
+#sorted_vect[:,range(num)]
 
 #mapping = None
 #cutting last low signifant eigen_faces
@@ -84,47 +102,61 @@ train_vectors = sorted_vect[:,range(num)]
 
 ###### PCA of test
 
-cov_matrix = np.cov(test_matrix)
-[evals, evect] = np.linalg.eig(cov_matrix)
+#cov_matrix = np.cov(test_matrix)
+#[evals, evect] = np.linalg.eig(cov_matrix)
 
-sorted_args = np.argsort(evals)
-sorted_args = sorted_args[::-1]
-sorted_vals = np.sqrt(np.sort(evals))
-sorted_vect = evect[:,sorted_args]
-test_vectors = sorted_vect[:,range(num)]
+#sorted_args = np.argsort(evals)
+#sorted_args = sorted_args[::-1]
+#sorted_vals = np.sqrt(np.sort(evals))
+#sorted_vect = evect[:,sorted_args]
+
+test_vectors = pca.transform(test_matrix)
+
+clf = SVC(kernel="poly")
+clf.fit(train_vectors, train_set_names)
+
+print clf.predict(test_vectors)
+print clf.score(test_vectors, test_set_names)
+
+#clf.predict_proba(...)
+#clf.predictlog_proba(....)
+
+
+
+#sorted_vect[:,range(num)]
 
 ###### computing distancies
 
-results = np.zeros((test_counter, img_counter), dtype=np.float64)
+#results = np.zeros((test_counter, img_counter), dtype=np.float64)
 
-for index, train_vector in enumerate(train_vectors):
-    if index >= len(train_set_list):
-        break
-    for i, test_vector in enumerate(test_vectors):
-        dist = np.linalg.norm(train_vector - test_vector)
-        results[i][index] = dist
+#for index, train_vector in enumerate(train_vectors):
+#    if index >= len(train_set_list):
+#        break
+#    for i, test_vector in enumerate(test_vectors):
+#        dist = np.linalg.norm(train_vector - test_vector)
+#        results[i][index] = dist
 #        print str((index / 6) + 1) + "/" + str((index % 6) + 1) + " vs " + str((i / 2) + 1) + "/" + str((i % 2) + 1) + " --> " + str(dist) 
 
 #print results
 
-evaluate = {}
+#evaluate = {}
 
-for i in range(len(results)):
-    index = train_set_names[np.argmin(results[i])]
-    value = np.min(results[i])
-    evaluate[i] = (index, value)
+#for i in range(len(results)):
+#    index = train_set_names[np.argmin(results[i])]
+#    value = np.min(results[i])
+#    evaluate[i] = (index, value)
 
-counter = 0
-for i in range(len(evaluate)):
-    s = ""
-    if (i / 2) != evaluate[i][0]:
-        s = "-->miss<--"
-    else:
-        s = "-->ok<--"
-        counter += 1
-    print str(i+1) + " --> guess: " + str(evaluate[i][0]) + " reality: " + str((i/2) + 1) + " " + s
-    
-print "Rate is: " + str(float(len(evaluate)) / float(counter))
+#counter = 0
+#for i in range(len(evaluate)):
+#    s = ""
+#    if (i / 2) != evaluate[i][0]:
+#        s = "-->miss<-- "
+ #   else:
+ ##       s = "-->ok<-- "
+ #       counter += 1
+ #   print str(i+1) + " --> guess: " + str(evaluate[i][0]) + " reality: " + str((i/2) + 1) + " " + s + str(evaluate[i][1])
+
+#print "counter: " + str(counter)  
 
 #eigen_faces = np.dot(sorted_vect, mapping) - train_matrix
 
